4be1c62d52748c665c3cdb197f27004f97c4b47f
khayarihasna055-hash <khayarihasna055@gmail.com>
2026-04-23 20:59:37 +0200

    clean: remove backend from frontend branch

--- backend/reports/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Report
-
-@admin.register(Report)
-class ReportAdmin(admin.ModelAdmin):
-    list_display = ['property', 'month', 'year', 'generated_at']
-    list_filter = ['property', 'year']
-    readonly_fields = ['generated_at']
