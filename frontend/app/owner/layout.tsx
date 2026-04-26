d097b5feade291b0ffb7c28614d183f042e2fb9a
Hasna KHAYARI <khayarihasna055@gmail.com>
2026-04-26 21:02:46 +0200

    Remove commented  text from layout.tsx

--- frontend/app/owner/layout.tsx
+++ frontend/app/owner/layout.tsx
@@ -40,7 +40,7 @@ export default function OwnerLayout({ children }: { children: React.ReactNode })
 
   return (
     <div className="flex h-screen bg-[#f9fafb]">
-      {/* Sidebar - ديما كاين في كاع الصفحات */}
+      
       <aside className="w-60 bg-white border-r border-gray-100 flex flex-col shadow-sm z-20">
         <div className="p-6 border-b border-gray-50">
           <div className="flex items-center gap-3">
@@ -85,7 +85,7 @@ export default function OwnerLayout({ children }: { children: React.ReactNode })
 
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col overflow-hidden">
-        {/* Navbar - الفوقاني نقي دبا (غير البحث والبروفايل) */}
+       
         <header className="bg-white border-b border-gray-100 px-8 py-4 flex justify-between items-center z-10">
           
           <div className="relative w-96">
@@ -115,7 +115,7 @@ export default function OwnerLayout({ children }: { children: React.ReactNode })
           </div>
         </header>
 
-        {/* Content - هنا فين كيتعرض داكشي اللي في page.tsx */}
+        
         <main className="flex-1 overflow-auto bg-[#f9fafb]">
           {children}
         </main>
