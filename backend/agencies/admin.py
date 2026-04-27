2d4346a3a1277bf7afaecacbe31b3116f31ac44c
hbhabiba121-hash <hbhabiba121@gmail.com>
2026-04-27 16:43:09 +0100

    remove backend code to keep frontend clean

--- backend/agencies/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Agency
-
-@admin.register(Agency)
-class AgencyAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'phone', 'created_at']
-    search_fields = ['name', 'email']
--- backend/financials/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FinancialRecord
-
-@admin.register(FinancialRecord)
-class FinancialRecordAdmin(admin.ModelAdmin):
-    list_display = ['property', 'month', 'year', 'revenue', 'expenses', 'commission_rate']
-    list_filter = ['property', 'year']
-    readonly_fields = ['created_at', 'updated_at']
